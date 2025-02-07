e19684fb610fe6d35276e9676dc5e33c85b9417a
Matthias Alzate <matthias.alzate@elev.ga.ntig.se>
2025-02-07 22:59:41 +0100

    done ish, just polishing time left and maybe add some more stuff

--- fight2/Program.cs
+++ fight2/Program.cs
@@ -11,8 +11,9 @@ p1.Acc = 0;
 p1.Dex = 0;
 p1.Stat = 20;
 
+// all enemies stats are the same for now
 Characters e1 = new();
-e1.Name = "Jax";
+e1.Name = "Kayn";
 e1.Vt = 2;
 e1.Atk = 3;
 e1.Def = 0;
@@ -23,7 +24,27 @@ e1.Hp = 100 + (10 * e1.Vt);
 e1.MaxHp = e1.Hp;
 
 Characters e2 = new();
+e2.Name = "Jackie";
+e2.Vt = 2;
+e2.Atk = 3;
+e2.Def = 0;
+e2.Spd = 8;
+e2.Acc = 3;
+e2.Dex = 0;
+e2.Hp = 100 + (10 * e2.Vt);
+e2.MaxHp = e2.Hp;
+
 Characters e3 = new();
+e3.Name = "Evelynn";
+e3.Vt = 2;
+e3.Atk = 3;
+e3.Def = 0;
+e3.Spd = 8;
+e3.Acc = 3;
+e3.Dex = 0;
+e3.Hp = 100 + (10 * e3.Vt);
+e3.MaxHp = e3.Hp;
+
 
 // int randomChoice;
 int storyPoint = 0;
@@ -45,6 +66,7 @@ bool ContainsNumbers(string input)
 
 while (keepPlaying == "yes")
 {
+    // ------- NAME CHECK -------
     // just happens when you start the program or decide to reset
     if (storyPoint == 0)
     {
@@ -77,6 +99,8 @@ while (keepPlaying == "yes")
         p1 = FirstStats(p1);
     }
 
+    // ------- BEFORE AND DURING FIGHT -------
+    // just checks where you are in the story and makes you fight the correct one
     if (storyPoint == 0)
     {
         p1 = betweenFight(p1, e1);
@@ -93,6 +117,7 @@ while (keepPlaying == "yes")
         (p1, e3, storyPoint) = fight(p1, e3, storyPoint);
     }
 
+    // ------- STORY PROGRESSION CHECK -------
     // when you finish the game (3) or lose one fight (4) you get here
     if (storyPoint == 4 || storyPoint == 3)
     {
@@ -108,7 +133,7 @@ while (keepPlaying == "yes")
     Console.Clear();
 }
 
-// ==================== METODER ====================
+// ==================== METHODS ====================
 
 
 // STORY PROGRESSION
@@ -117,27 +142,27 @@ static int story(int story)
 
     if (story == 0)
     {
-        Console.WriteLine("placeholder start story\n");
+        Console.WriteLine("you got thrown into prison and to get free you have to win 3 fights. first fight is against Kayn\n");
         Console.ReadLine();
     }
     else if (story == 1)
     {
-        Console.WriteLine("placeholder win first fight");
+        Console.WriteLine("you won your first fight against Kayn, now you have to fight Jackie ");
         Console.ReadLine();
     }
     else if (story == 2)
     {
-        Console.WriteLine("placeholder win second fight");
+        Console.WriteLine("you won your second fight against Jackie, now you have to fight Evelynn");
         Console.ReadLine();
     }
     else if (story == 3)
     {
-        Console.WriteLine("placeholder win last fight");
+        Console.WriteLine("You won your last fight against Evelynn and are now free to go! CONGRATS!");
         Console.ReadLine();
     }
     else if (story == 4)
     {
-        Console.WriteLine("Placeholder losing anytime");
+        Console.WriteLine("You win some, you lose some. this time you lost your life. Game over.");
         Console.ReadLine();
     }
     return story;
@@ -418,27 +443,60 @@ static Characters StatPoints(Characters player)
 
 static (Characters player, Characters enemy, int story) fight(Characters player, Characters enemy, int story)
 {
+    Random generator = new Random();
+    int enemySpeed;
+    int playerSpeed;
+
     while (player.Hp > 0 && enemy.Hp > 0)
     {
         Console.Clear();
         Console.WriteLine("======= NEW ROUND =======");
         Console.WriteLine($"{player.Name}: {player.Hp} Hp  |  {enemy.Name}: {enemy.Hp} Hp\n");
 
-        // going to impliment speed check later
-        (player, enemy) = playerAttack(player, enemy);
-        (player, enemy) = enemyAttack(player, enemy);
+        enemySpeed = generator.Next(1, 101) + (enemy.Spd * 5);
+        playerSpeed = generator.Next(1, 101) + (player.Spd * 5);
 
-
-
-
-        //  ==================== ENEMY ATTACK ========================
-
-        // gives the enemy a randon choice
-
-
-
+        // if enemy speed is higher than player speed, the enemy attacks first
+        if (enemySpeed > playerSpeed)
+        {
+            Console.WriteLine($"The enemy attacks first");
+            Console.WriteLine("ENTER to continue");
+            Console.ReadLine();
+            (player, enemy) = enemyAttack(player, enemy);
+            (player, enemy) = playerAttack(player, enemy);
+        }
+        else
+        {
+            Console.WriteLine($"you attacks first");
+            Console.WriteLine("ENTER to continue");
+            Console.ReadLine();
+            (player, enemy) = playerAttack(player, enemy);
+            (player, enemy) = enemyAttack(player, enemy);
+        }
     }
-    player.Hp = 100 + (10 * player.Vt);
+
+    // ------- WHO WIN FIGHT CHECK -------
+    Console.WriteLine("\n======== THE FIGHT IS OVER ========");
+
+    if (player.Hp == 0 && enemy.Hp == 0)
+    {
+        Console.WriteLine("BOTH DIED\n");
+        story = 4;
+    }
+    else if (player.Hp == 0)
+    {
+        Console.WriteLine($"{player.Name} died, {enemy.Name} won!\n");
+        story = 4;
+    }
+    else
+    {
+        Console.WriteLine($"{enemy.Name} died, {player.Name} won!\n");
+        story++;
+    }
+    Console.WriteLine("ENTER to continue");
+    Console.ReadLine();
+    player.Hp = player.MaxHp;
+
     return (player, enemy, story);
 }
 
@@ -464,7 +522,7 @@ static (Characters player, Characters enemy) playerAttack(Characters player, Cha
     // if the answer is not one of the options in the array "acceptable" it asks you to try again
     while (!acceptable.Contains(attackChoice))
     {
-        Console.WriteLine("Okänt Svar, försök igen\n");
+        Console.WriteLine("Unknown answer, please try again\n");
         attackChoice = Console.ReadLine();
     }
 
@@ -486,7 +544,7 @@ static (Characters player, Characters enemy) playerAttack(Characters player, Cha
             enemy.Hp -= player.Dmg;
             enemy.Hp = Math.Max(0, enemy.Hp);
             Console.WriteLine($"{player.Name} uses light attack!");
-            Console.WriteLine($"{player.Name} does {player.Dmg} to {enemy.Name}\n");
+            Console.WriteLine($"{player.Name} does {player.Dmg} damage to {enemy.Name}\n");
         }
     }
     // if you chhose 2 or b the user tries a heavy attack
@@ -502,7 +560,7 @@ static (Characters player, Characters enemy) playerAttack(Characters player, Cha
             enemy.Hp -= player.Dmg;
             enemy.Hp = Math.Max(0, enemy.Hp);
             Console.WriteLine($"{player.Name} uses heavy attack!");
-            Console.WriteLine($"{player.Name} does {player.Dmg} to {enemy.Name}\n");
+            Console.WriteLine($"{player.Name} does {player.Dmg} damage to {enemy.Name}\n");
         }
     }
     // if you choose 3 or c the user gets extra dodge chance
@@ -518,6 +576,8 @@ static (Characters player, Characters enemy) playerAttack(Characters player, Cha
         player.Hp += healing;
         Console.WriteLine($"{player.Name} healed {healing} Hp");
     }
+    Console.WriteLine("ENTER to continue");
+    Console.ReadLine();
 
     return (player, enemy);
 
@@ -529,6 +589,8 @@ static (Characters player, Characters enemy) enemyAttack(Characters player, Char
     Random generator = new Random();
     int accuracy = generator.Next(1, 101);
     int randomChoice;
+    int healing;
+    enemy.ExtraDodge = 0;
 
     if (enemy.Hp < enemy.MaxHp / 3) // when hp is 
     {
@@ -543,22 +605,53 @@ static (Characters player, Characters enemy) enemyAttack(Characters player, Char
         randomChoice = generator.Next(2);
     }
 
+    // enemy using light attack
     if (randomChoice == 0)
     {
-
+        if (accuracy > 80 - enemy.Acc + player.Dex + player.ExtraDodge)
+        {
+            Console.WriteLine($"{player.Name} missed their attack\n");
+        }
+        else
+        {
+            enemy.Dmg = generator.Next(5 + player.Atk - enemy.Def, 21 + player.Atk - player.Def);
+            player.Hp -= enemy.Dmg;
+            player.Hp = Math.Max(0, player.Hp);
+            Console.WriteLine($"{enemy.Name} uses light attack!");
+            Console.WriteLine($"{enemy.Name} does {enemy.Dmg} damage to {player.Name}\n");
+        }
     }
+    // enemy using heavy attack
     else if (randomChoice == 1)
     {
-
+        if (accuracy > 30 - enemy.Acc + player.Dex + player.ExtraDodge)
+        {
+            Console.WriteLine($"{enemy.Name} missed their attack\n");
+        }
+        else
+        {
+            enemy.Dmg = generator.Next(10 + player.Atk - enemy.Def, 41 + (player.Atk * 2) - player.Def);
+            player.Hp -= enemy.Dmg;
+            player.Hp = Math.Max(0, player.Hp);
+            Console.WriteLine($"{enemy.Name} uses light attack!");
+            Console.WriteLine($"{enemy.Name} does {enemy.Dmg} damage to {player.Name}\n");
+        }
     }
+    // enemy using dodge
     else if (randomChoice == 2)
     {
-
+        enemy.ExtraDodge = enemy.Dex;
+        Console.WriteLine($"{enemy.Name} prepares to dodge {player.Name} next action");
     }
+    // enemy using rest
     else if (randomChoice == 3)
     {
-        
+        healing = generator.Next(enemy.MaxHp / 7, enemy.MaxHp / 4 + 1);
+        enemy.Hp += healing;
+        Console.WriteLine($"{enemy.Name} healed {healing} Hp");
     }
+    Console.WriteLine("ENTER to continue");
+    Console.ReadLine();
 
     return (player, enemy);
 }
